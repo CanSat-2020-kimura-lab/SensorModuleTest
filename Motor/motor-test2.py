d14783165aa9e764cde780253ac2731c3fcda285
R-Hiraoka <65853385+R-Hiraoka@users.noreply.github.com>
2020-07-07 14:48:19 +0900

    Update and rename moter-test2.py to motor-test2.py

--- Motor/motor-test2.py
+++ Motor/motor-test2.py
@@ -10,9 +10,9 @@ MODE = 9
 STBY = 11
 
 pi1 = pigpio.pi()
-pi1.set_mode(Ena1, pigpio.OUTPUT)
+pi1.set_mode(Ena1,pigpio.OUTPUT)
 pi1.set_mode(Pha1,pigpio.OUTPUT)
-pi1.set_mode(Ena2, pigpio.OUTPUT)
+pi1.set_mode(Ena2,pigpio.OUTPUT)
 pi1.set_mode(Pha2,pigpio.OUTPUT)
 pi1.set_mode(MODE,pigpio.OUTPUT)
 pi1.set_mode(LARGE,pigpio.OUTPUT)
@@ -36,25 +36,29 @@ def set_const(pi1,c, d, e, t):
 
 try:
 	set_motor_r(pi1, 0, 0, 0.5) # stop (neutral)
-  set_motor_l(pi2, 0, 0, 0.5) # stop (neutral)
+	set_motor_l(pi2, 0, 0, 0.5) # stop (neutral)
 	set_const(pi1,1, 0, 1, 0.3)
 	for i in range(2):
+		print("normal rotation r")
 		set_motor_r(pi1, 1, 1, 2.0) # normal rotation
 		set_motor_r(pi1, 0, 0, 1.0) # stop
+	for i in range(2):
+		print("normal rotation l")
     		set_motor_l(pi1, 1, 1, 2.0) # normal rotation
 		set_motor_l(pi1, 0, 0, 1.0) # stop
-
-
 	for i in range(2):
+		print("reverse rotation r")
 		set_motor_r(pi1, 1, 0, 2.0) # reverse rotation
 		set_motor_r(pi1, 0, 0, 1.0) # stop
+	for i in range(2):
+		print("reverse rotation l")
    	        set_motor_l(pi1, 1, 0, 2.0) # reverse rotation
 		set_motor_l(pi1, 0, 0, 1.0) # stop
 
 except KeyboardInterrupt:
 	print ("done!")
 	set_moter(pi1,0, 0, 0.5)
-
+print("motor-test2 has finished")
 set_motor_r(pi1, 0, 0, 0.5) # stop (neutral)
 set_motor_l(pi1, 0, 0, 0.5) # stop (neutral)
 pi1.set_mode(Ena1, pigpio.INPUT)
