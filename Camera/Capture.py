2cc395e4e11e1be73d8388791ad458cbf3d40b51
R-Hiraoka <65853385+R-Hiraoka@users.noreply.github.com>
2020-08-02 09:48:52 +0900

    Update Capture.py

--- Camera/Capture.py
+++ Camera/Capture.py
@@ -25,7 +25,7 @@ def Capture(path, width = 320, height = 240):
 if __name__ == "__main__":
 	try:
 		for i in range(3):
-			photoName = Capture("home/git/photo/photo", 160, 120)
+			photoName = Capture("/home/git/photo/photo", 160, 120)
 			print(photoName)
 	except KeyboardInterrupt:
 		print('stop')
